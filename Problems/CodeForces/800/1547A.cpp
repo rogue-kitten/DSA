bf6e3a6016876c26f27212a06386c5961584b355
Nash <110938199+RougeKitten@users.noreply.github.com>
2022-08-11 02:50:16 +0530

    Create 1547A.cpp

---
+++ Problems/CodeForces/800/1547A.cpp
@@ -0,0 +1,24 @@
+// link to problem -> https://codeforces.com/problemset/problem/1547/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		vector<int> a(2), b(2), f(2);
+		cin.get();
+		cin >> a[0] >> a[1];
+		cin >> b[0] >> b[1];
+		cin >> f[0] >> f[1];
+
+		int ans = abs(a[0] - b[0]) + abs(a[1] - b[1]);
+
+		if ((a[0] == b[0] && a[0] == f[0] && min(a[1], b[1]) < f[1] && f[1] < max(a[1], b[1]))
+                || (a[1] == b[1] && a[1] == f[1] && min(a[0], b[0]) < f[0] && f[0] < max(a[0], b[0])))
+            ans += 2;
+        cout << ans << endl;
+	}
+}

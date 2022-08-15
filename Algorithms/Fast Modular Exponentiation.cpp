5f3fdccecdef3bac50abc65f21d177f6c706156a
Nash <110938199+RougeKitten@users.noreply.github.com>
2022-08-16 02:29:01 +0530

    Create Fast Modular Exponentiation.cpp

---
+++ Algorithms/Fast Modular Exponentiation.cpp
@@ -0,0 +1,18 @@
+ll mod = 1e9 + 7;
+
+ll solve(ll a, ll n){
+	if(a == 0)
+		return 0;
+	if(n == 0)
+		return 1;
+	if(n == 1)
+		return a;
+
+	ll x = (solve(a, n/2) % mod);
+	if(n&1)
+		return (a*((x*x)%mod))%mod;
+
+	else
+		return (x*x)%mod;
+}
+

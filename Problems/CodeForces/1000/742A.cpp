7ca76c4c5f65f40ad77d49efaaee49116d17683a
Nash <110938199+RougeKitten@users.noreply.github.com>
2022-08-27 16:52:16 +0530

    Create 742A.cpp

---
+++ Problems/CodeForces/1000/742A.cpp
@@ -0,0 +1,56 @@
+// link to the problem -> https://codeforces.com/problemset/problem/742/A
+
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define mod 1000000007
+#define vi vector<int>
+#define vll vector<long long>
+#define vull vector<unsigned long long>
+#define vvi vector<vector<int>>
+#define umii unordered_map<int, int>
+#define umivi unordered_map<int, vector<int>>
+#define printall(a) for(auto i : a) cout << i << " "
+#define print(a) cout << a << endl
+#define pb push_back
+#define maxHeap  priority_queue<int>
+#define minHeap  priority_queue<int, vector<int>, greater<int> >
+
+ll gcd(ll a, ll b){ return b == 0 ? a : gcd(b, a%b); }
+
+ll modularMultiplication(ll a, ll b, ll m){
+	ll res = 0;
+	while(b > 0){
+		if(b&1ll)
+			res += a;
+		a *= 2;
+		b = b >> 1;
+		res %= m;
+		a %= m;
+	}
+	return res;
+}
+
+ll FastModularExp(ll a, ll b, ll m){
+	ll res = 1ll;
+
+	while(b > 0){
+		if(b&1ll){
+			res = modularMultiplication(res, a, m);
+		}
+		a = modularMultiplication(a, a, m);
+		b = b >> 1;
+	}
+	return res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll n;
+	cin >> n;
+
+	print(FastModularExp(8, n, 10));
+}

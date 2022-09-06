a307ef9a8b5ff3baf0d876d4b61c03eb3d51c6a9
Nash <110938199+RougeKitten@users.noreply.github.com>
2022-09-06 16:35:01 +0530

    Update 852.cpp

--- Problems/Leetcode/852.cpp
+++ Problems/Leetcode/852.cpp
@@ -3,21 +3,17 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        int s = 1, e = n - 2, m, ans  = -1;
-        if(s == e)
-            return 1;
-        
+       int n = arr.size();
+        int s = 0, e = n - 1, m, ans = -1;
         while(s <= e){
-            m = s + (e - s)/ 2;
-            if(arr[m] > arr[m + 1] && arr[m] > arr[m -1])
-                return m;
-            if(arr[m] > arr[m + 1] && arr[m] < arr[m -1])
-                e = m - 1;
-            else
+            m = s + (e - s) / 2;
+            if(m == 0 || arr[m] > arr[m - 1]){
+                ans = m;
                 s = m + 1;
+            }
+            else
+                e = m - 1;
         }
-        
-        return -1;
+        return ans;
     }
 };
